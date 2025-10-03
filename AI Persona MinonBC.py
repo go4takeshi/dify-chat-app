cc36970c901e848eec12103e636b98e3d411dc76
go4takeshi <111494442+go4takeshi@users.noreply.github.com>
2025-10-04 03:27:56 +0900

    Update AI Persona MinonBC.py

--- AI Persona MinonBC.py
+++ AI Persona MinonBC.py
@@ -163,7 +163,8 @@ if qp.get("page") and st.session_state.page != qp.get("page"):
 
 # ========== STEP 1: ログイン ==========
 if st.session_state.page == "login":
-    st.title("ミノンBCファンAIと\nグループでチャット")
+    st.title("ミノンBCファンAIと")
+    st.title("グループでチャット")
 
     with st.form("user_info_form"):
         name = st.text_input("あなたの表示名", value=st.session_state.name or "")
@@ -379,6 +380,7 @@ else:
         st.session_state.cid = ""
         st.query_params.clear()
         st.rerun()
+
 
 
 
